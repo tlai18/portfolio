81f89f4483495cfdd3aaab1ab370a8706917e3f0
Thomas Lai <thomas.lai@tufts.edu>
2025-07-27 22:42:11 -0400

    Make contact and footer mobile responsive

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ScrollAnimateDirective } from '../scroll-animate.directive';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ScrollAnimateDirective],
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
